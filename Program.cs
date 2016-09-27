59bcacb56dadd68250257a5dcf2ec42b8bc2f9eb
RiddhiSolanki <riddhisolanki91@gmail.com>
2016-09-27 16:18:14 -0400

    Lab 4

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Factorial Calculator");
+
+            bool answer = true;
+
+            while (answer)
+            {
+
+                Console.WriteLine("Enter an integer that's greater than 0 but less than 10");
+                int num01 = int.Parse(Console.ReadLine());
+                long factorial = 1;
+                for (int i = 1; i <= num01; i++)
+
+                {
+                    factorial = factorial * i;
+                    Console.WriteLine("The factorial of " + i + " is " + factorial);
+
+                }
+                Console.WriteLine("Do you want to continue? (y/n)");
+                string con = Console.ReadLine();
+                if (con == "y")
+
+                { answer = true; }
+
+                else
+                {
+                    answer = false;
+                    Console.WriteLine("Thank you and Take care");
+                }
+
+
+
+
+
+            }
+
+        }
+
+
+
+    }
+}
+
